88318a1890269854b54b44baf0f5b1b33817f91c
xMeowtch <lys8167@gmail.com>
2025-09-27 19:14:14 +0900

    week2-하단 탭 구현

--- yoonseo/app/src/main/java/com/example/yoonseo/MainActivity.kt
+++ yoonseo/app/src/main/java/com/example/yoonseo/MainActivity.kt
@@ -1,47 +1,84 @@
 package com.example.yoonseo
 
+import BrowseFragment
+import HomeFragment
+import LibraryFragment
+import SearchFragment
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.yoonseo.ui.theme.MyApplicationTheme
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.yoonseo.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBottomNavigation()
+
+        binding.mainPlayerCl.setOnClickListener {
+            TODO()
+            startActivity(intent)
+        }
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val sysBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            // 루트에 안전패딩을 주면 하위 자식들이 자연스럽게 안전영역 안에 배치됨
+            v.setPadding(v.paddingLeft, sysBars.top, v.paddingRight, sysBars.bottom)
+
+            // 혹은 바텀 내비만 따로 처리하고 싶으면:
+            // binding.mainBnv.updatePadding(bottom = sysBars.bottom)
+
+            insets
+        }
+
+    }
+
+    private fun initBottomNavigation(){
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, HomeFragment())
+            .commitAllowingStateLoss()
+
+        binding.mainBnv.setOnItemSelectedListener{ item ->
+            when (item.itemId) {
+
+                R.id.homeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, HomeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.browseFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, BrowseFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.searchFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, SearchFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.libraryFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, LibraryFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
                 }
             }
+            false
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
     }
 }
